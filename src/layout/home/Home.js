2bee9879b7b231fa060976c3afb3c283e4f8c467
jawariaalvi32 <jawariaalvi32@gmail.com>
2021-02-04 23:46:11 +0500

    layout

--- src/layout/home/Home.js
+++ src/layout/home/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Contact from '../../components/contact/Contact'
 import Intro from '../../components/intro/Intro'
 import Skills from '../../components/skills/Skills'
+import Projects from '../projects/Projects'
 
 const Home = () => {
     return (
         <div>
             <Intro/>
             <Skills/>
+            <Projects/>
             <Contact/>
         </div>
     )
